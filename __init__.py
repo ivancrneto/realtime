71d524d79355fb3654f7639c12ba1b63438c9b82
Ivan Neto <ivan.cr.neto@gmail.com>
2013-04-06 19:22:54 -0300

    make home page working

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-import core
-app.register_blueprint(core.mod)
--- realtime/__init__.py
+++ realtime/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
 
-import core
-app.register_blueprint(core.mod)
+from core.views import general
+app.register_blueprint(general.mod)
