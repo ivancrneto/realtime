48014daa2d180ad9832af5056b5c42db14a61edc
Ivan Neto <ivan.cr.neto@gmail.com>
2013-04-09 02:20:06 -0300

    basic crud for word

--- realtime/core/views.py
+++ realtime/core/views.py
@@ -1,8 +1,23 @@
-from flask import Blueprint, request, render_template
+from flask import Blueprint, request, render_template, jsonify, \
+    Response
+from realtime.database import Word, db_session
+import simplejson as json
 
 mod = Blueprint('core', __name__)
 
 @mod.route('/')
 def home():
-  # return 'Hello world'
-  return render_template('core/index.html')
+    return render_template('core/index.html')
+
+@mod.route('/words/add', methods=['POST'])
+def add_word():
+    word = Word(**request.form)
+    db_session.add(word)
+    db_session.commit()
+    return jsonify(word=word.to_json())
+
+@mod.route('/words/get', methods=['GET'])
+def get_words():
+    words = Word.query.all()
+    res = json.dumps([w.to_json() for w in words])
+    return Response(res, mimetype='application/json')
